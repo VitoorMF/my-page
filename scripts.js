e41c92966ab9f1fe29897faeef814e82fd6c261f
Vitor MF <vitorfreire419@gmail.com>
2025-01-08 11:35:24 -0300

    add js and css

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function getLocation(coordinates, commands) {
+    let finalCoordinates = [];
+  
+    for(let i = 0; i <= commands.lenght; i++){
+      if(commands[i] === 'forward'){}
+      console.log(i)
+    }
+  }
+
+
+
+  getLocation([0, 0], ['forward', 'right']);
+  
